be57e28a5b1be3b1e0d9992f0cb6837223691832
tnb47 <yts2019hc2047@gmail.com>
2025-05-05 16:08:03 +0900

    Delete完了

--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Task } from "./input/task";
-export const Tasks = ({ taskList ,setTaskList}) => {
+export const Tasks = ({ taskList, setTaskList }) => {
   return (
     <div className="tasks">
       {taskList.map((task, index) => (
         <div key={index}>
-          <Task key={task.id} task={task} tasklist={taskList} setTaskList={setTaskList}/>
+          <Task
+            key={task.id}
+            task={task}
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
         </div>
       ))}
     </div>
--- src/components/task/input/task.jsx
+++ src/components/task/input/task.jsx
@@ -2,26 +2,17 @@ import React from "react";
 
 export const Task = ({ task, taskList, setTaskList }) => {
   const handleDelete = (taskId) => {
-    console.log("handleDelete called with taskId:", taskId);
-    console.log("Current taskList:", taskList);
-    
     if (taskList) {
       setTaskList(taskList.filter((t) => t.id !== taskId));
-      console.log("Updated taskList:", taskList.filter((t) => t.id !== taskId));
-    } else {
-      console.log("taskList is undefined");
     }
   };
 
-  console.log("Task component rendered with task:", task);
-  
   return (
     <div className="taskBox">
       <p className="taskText">{task.text}</p>
       <button 
         className="taskTrashButton" 
         onClick={() => {
-          console.log("Button clicked");
           handleDelete(task.id);
         }}
       >
